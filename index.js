e5054b4d55c7e9ce7aac8182adb752fec462904b
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-12-21 14:36:39 -0500

    commit tree 58514baff2ed43193b79c3187037cb9d40e3440f

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// const totalBatteries = (currentBatch, nextBatch) => currentBatch + nextBatch;
+// let result = batteryBatches.reduce(totalBatteries);
+
+
+const totalBatteries = batteryBatches.reduce(function() {
+    let sum = 0;
+    for(let i = 0; i < batteryBatches.length; i++) {
+        sum += batteryBatches[i];
+    }
+    return sum;
+})
